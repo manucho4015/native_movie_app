e0c4875f098f347c5da49c5396d11f7302bb5350
Emmanuel Ochieng <eochieng16@gmail.com>
2025-11-27 14:17:18 +0300

    remove unnecessary logging

--- services/api.ts
+++ services/api.ts
@@ -27,10 +27,3 @@ export const fetchMovies = async ({ query }: { query: string }) => {
     return data.results;
 }
 
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-
-
-// fetch(url, options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.error(err));
